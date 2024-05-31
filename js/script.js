a67aed3a4e8dfe95925fd76484fc1a2fa43644fb
Nurul Fadilah <150462453+Nufa-nur@users.noreply.github.com>
2024-05-31 20:28:01 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Customer Reviews
+//
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 30,
+  centeredSlides: true,
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+$(".datepicker").datepicker({
+  inline: true,
+});
